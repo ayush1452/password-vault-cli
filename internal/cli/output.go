a53a11c495a038554caa50783b113bac87891934
Ayush Sharma <ayush0810sharma@gmail.com>
2025-11-11 23:14:26 +0530

    fixed the unused function

--- internal/cli/output.go
+++ internal/cli/output.go
@@ -3,7 +3,6 @@ package cli
 import (
 	"fmt"
 	"io"
-	"os"
 )
 
 // writeOutput is a helper function to write output with error checking
@@ -13,9 +12,4 @@ func writeOutput(w io.Writer, format string, args ...interface{}) error {
 		return fmt.Errorf("failed to write output: %w", err)
 	}
 	return nil
-}
-
-// printStatus is a convenience wrapper for writing to stdout with error checking
-func printStatus(format string, args ...interface{}) error {
-	return writeOutput(os.Stdout, format, args...)
 }
