ffbd4761e1ed68cfcae37c5f3b2770b524e6bde6
Ayush Sharma <ayush0810sharma@gmail.com>
2026-02-13 01:32:42 +0530

    updating the testing function

--- testing_file.go
+++ testing_file.go
@@ -1,5 +1,46 @@
-package passwordvaultcliproject
+package main
+
+import (
+	"fmt"
+)
+
+var vault = make(map[string]string)
+
+// addPassword stores a password for a given service
+func addPassword(service string, password string) {
+	vault[service] = password
+	fmt.Println("Password added successfully.")
+}
+
+// getPassword retrieves a password for a given service
+func getPassword(service string) {
+	password, exists := vault[service]
+	if !exists {
+		fmt.Println("No password found for service:", service)
+		return
+	}
+	fmt.Println("Password for", service, "is:", password)
+}
+
+// listServices prints all stored services
+func listServices() {
+	if len(vault) == 0 {
+		fmt.Println("No services stored.")
+		return
+	}
+
+	fmt.Println("Stored services:")
+	for service := range vault {
+		fmt.Println("-", service)
+	}
+}
 
 func main() {
+	// Example usage
+	addPassword("gmail", "gmail_password_123")
+	addPassword("github", "github_password_456")
 
+	listServices()
+	getPassword("gmail")
+	getPassword("facebook")
 }
